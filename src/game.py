587b0e8da37d2c884cf05e46edce299f2792e3ff
Alisson <alissonvpb18@gmail.com>
2026-09-14 10:56:46 -0300

    Criando função para verificar se o tabuleiro está cheio

--- src/game.py
+++ src/game.py
@@ -2,3 +2,9 @@ def valid_move(board, row, column):
     if not board[row][column]:
         return True
     return False
+
+def full_board(board):
+    for row in board:
+        if not all(row):
+            return False
+    return True
